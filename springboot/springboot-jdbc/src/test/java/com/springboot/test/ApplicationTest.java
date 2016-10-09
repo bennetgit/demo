562b7734e06934b87e3c0bb07631777cef00199d
facheng.wang <facheng.wang@otms.cn>
2016-10-09 16:06:50 +0800

    Use java send mail to send email.

--- springboot/springboot-jdbc/src/test/java/com/springboot/test/ApplicationTest.java
+++ springboot/springboot-jdbc/src/test/java/com/springboot/test/ApplicationTest.java
@@ -4,9 +4,12 @@ import javax.annotation.Resource;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.springboot.jdbc.Application;
@@ -19,11 +22,25 @@ public class ApplicationTest {
     @Resource
     private UserService userService;
 
+    @Resource
+    private JavaMailSender mailSender;
+
     @Before
     public void setUp() {
         userService.deleteAllUsers();
     }
 
+    @Test
+    public void sendSimpleMail(){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("1092221803@qq.com");
+        message.setTo("facheng.wang@aliyun.com");
+        message.setSubject("测试邮件");
+        message.setText("测试邮件内容");
+        mailSender.send(message);
+    }
+
+    @Ignore
     @Test
     public void test() {
         userService.create("a", 1);
