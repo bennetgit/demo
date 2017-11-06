ebfaa10b1836794d3bb8e8840f5c5de08700fb47
facheng.wang <facheng.wang@otms.com>
2017-11-06 18:31:07 +0800

    user auth.

---
+++ spring/springboot-demo/src/main/java/spring/demo/service/impl/MyUserDetailsService.java
@@ -0,0 +1,15 @@
+package spring.demo.service.impl;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+/**
+ * Created by wangfacheng on 2017-11-06.
+ */
+public class MyUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
