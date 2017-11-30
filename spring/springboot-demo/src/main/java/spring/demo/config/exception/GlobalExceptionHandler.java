862787a3414a4307a9d8aa7a08b0381e34cd5482
facheng.wang <facheng.wang@otms.com>
2017-11-30 14:44:34 +0800

    error setting.

---
+++ spring/springboot-demo/src/main/java/spring/demo/config/exception/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package spring.demo.config.exception;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by facheng on 17-11-30.
+ */
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    public static final String DEFAULT_ERROR_VIEW = "error";
+
+    public static final String DEFAULT_ERROR_MESSAGE = "error_message";
+
+    @ExceptionHandler
+    public ModelAndView defaultErrorHandler(HttpServletRequest request, Exception e) {
+
+        LOGGER.error(e.getMessage(), e);
+        ModelAndView errorModelAndView = new ModelAndView(DEFAULT_ERROR_VIEW);
+        Map<String, Object> model = errorModelAndView.getModel();
+        model.put(DEFAULT_ERROR_MESSAGE, e);
+        return errorModelAndView;
+    }
+}
