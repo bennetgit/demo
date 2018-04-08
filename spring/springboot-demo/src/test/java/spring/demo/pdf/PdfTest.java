b69db5c633de0c38b79d1f53fab668c4fc6c1aa5
facheng.wang <facheng.wang@otms.com>
2018-04-08 12:55:05 +0800

    logo position modify.

--- spring/springboot-demo/src/test/java/spring/demo/pdf/PdfTest.java
+++ spring/springboot-demo/src/test/java/spring/demo/pdf/PdfTest.java
@@ -27,7 +27,7 @@ public class PdfTest {
     @Test
     public void createPdfTest() throws Exception {
 
-        String outfile = "/Users/feng/backup/pdf/123.pdf";
+        String outfile = "/home/facheng/backup/pdf/123.pdf";
         // String fontPath =
         // "/home/facheng/source/fc/demo/spring/springboot-demo/src/test/resources/font";
         String fontPath = getClass().getResource("/font").getFile();
@@ -38,7 +38,7 @@ public class PdfTest {
 
         // 设置要解析的模板所在的目录，并加载模板文件
         config.setDirectoryForTemplateLoading(
-                new File("/Users/feng/sources/myself/demo/spring/springboot-demo/src/test/resources"));
+                new File("/home/facheng/source/fc/demo/spring/springboot-demo/src/test/resources"));
         // 设置包装器，并将对象包装为数据模型
         config.setObjectWrapper(new DefaultObjectWrapperBuilder(Configuration.VERSION_2_3_23).build());
         // 获取模板,并设置编码方式，这个编码必须要与页面中的编码格式一致
