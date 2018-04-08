a36f2ddb26402d21aea242a052ffe9d7c1587c89
facheng.wang <facheng.wang@otms.com>
2018-04-08 14:01:46 +0800

    lock test.

---
+++ spring/springboot-demo/src/test/java/spring/demo/lock/LockTest.java
@@ -0,0 +1,44 @@
+package spring.demo.lock;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.junit.Test;
+
+/**
+ * Created by wangfacheng on 2018-04-08.
+ */
+public class LockTest {
+
+    @Test
+    public void testLock() throws InterruptedException {
+
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+
+        Thread t1 = new Thread(() -> {
+            System.out.println("t1 start request String.class monitor object");
+            synchronized (String.class) {
+                System.out.println("t1 lock String, sleep 10s");
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            countDownLatch.countDown();
+        });
+
+        Thread t2 = new Thread(() -> {
+            System.out.println("t2 start request String.class monitor object");
+            synchronized (String.class) {
+                System.out.println("t2 request lock String.class monitor success");
+            }
+
+            countDownLatch.countDown();
+        });
+
+        t1.start();
+        t2.start();
+
+        countDownLatch.await();
+    }
+}
